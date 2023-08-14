20c3e90ccf8f5495ac6370fd225a384e3ac0b37f
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-08-14 16:06:35 +0600

    repo update

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port = 5000;
+
+async function maker() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+maker();
